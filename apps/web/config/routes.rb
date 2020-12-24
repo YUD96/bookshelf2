fb08c86449f321ba5271770ee71e8c60847d4ba8
YUD96 <yudai000916@gmail.com>
2020-12-24 22:37:14 +0900

    first commit

---
+++ apps/web/config/routes.rb
@@ -0,0 +1,12 @@
+# Configure your routes here
+# See: https://guides.hanamirb.org/routing/overview
+#
+# Example:
+# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
+get '/books', to: 'books#index'
+get '/books/new', to: 'books#new'
+post '/books', to: 'books#create'
+delete '/books/:id', to: 'books#destroy'
---
+++ apps/web/controllers/books/destroy.rb
@@ -0,0 +1,12 @@
+module Web
+  module Controllers
+    module Books
+      class Destroy
+        include Web::Action
+
+        def call(params)
+        end
+      end
+    end
+  end
+end

f7b2d5bb71bd4896ff4276f3d0573aa1aa13a638
YUD96 <yudai000916@gmail.com>
2021-01-03 21:26:35 +0900

    css適用

--- apps/web/views/sessions/new.rb
+++ apps/web/views/sessions/new.rb
@@ -3,12 +3,6 @@ module Web::Views::Sessions
 		include Web::View
 
 		def form 
-			form_for :session , '/login' do 
-				email_field :mail
-				text_field :password
-
-				submit 'POST'
-			end
 		end
   end
 end
